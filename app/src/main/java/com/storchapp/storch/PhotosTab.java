c792fff037c3f7aeebf4eed47426db9c097bc030
Kenan Soylu <ken.soylu@hotmail.com>
2017-03-04 16:34:44 +0300

    New Tab layout java class

--- app/src/main/java/com/storchapp/storch/PhotosTab.java
+++ app/src/main/java/com/storchapp/storch/PhotosTab.java
@@ -1,8 +1,17 @@
 package com.storchapp.storch;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by ken on 04.03.2017.
  */
 
-public class PhotosTab {
+public class PhotosTab extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.photos_tab, container, false);
+    }
 }
